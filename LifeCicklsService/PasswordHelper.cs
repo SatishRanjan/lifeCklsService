1b63e207c7c182d798ac4595f5dd30e5f9adcfa7
SatishRanjan <satishranjan.github@gmail.com>
2024-02-01 23:59:26 -0800

    Saving user profile and LifeCkl

---
+++ LifeCicklsService/PasswordHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+public class PasswordHasher
+{
+    public static string GetPasswordHash(string password)
+    {
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            // Convert the password string to a byte array
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+
+            // Compute the hash value of the byte array
+            byte[] hashBytes = sha256.ComputeHash(passwordBytes);
+
+            // Convert the hash byte array to a hexadecimal string
+            string hashedPassword = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+
+            return hashedPassword;
+        }
+    }
+
+    public static bool VerifyPassword(string enteredPassword, string storedHashedPassword)
+    {
+        // Hash the entered password using the same method used during registration
+        string enteredPasswordHash = GetPasswordHash(enteredPassword);
+
+        // Compare the entered password hash with the stored hashed password
+        return enteredPasswordHash.Equals(storedHashedPassword, StringComparison.OrdinalIgnoreCase);
+    }
+}
---
+++ LifeCklsModels/LifeCkl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeCklsModels
+{
+    public class LifeCkl
+    {
+        public string ProfileId { get; set; }
+        public List<string> Stories { get; set; }
+        public List<string> Connections { get; set; }
+        public List<string> Invites { get; set; }
+        public List<string> Messages { get; set; }
+    }
+}
